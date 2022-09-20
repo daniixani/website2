5e23086c650597cfd199945688b46f35cbfe8718
daniixani <89828237+daniixani@users.noreply.github.com>
2022-09-20 15:58:38 -0700

    svelte.misionx

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>import "../app.scss";</script><slot></slot>
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Mission X</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/competitions">Competitions</a>
+          </li>
+          <li class="nav=item">
+            <a class="nav-link" href="/cyberchats">Cyber Chats</a>
+          </li>
+          <a class="btn btn-primary" href="https://calendar.google.com/calendar/embed?src=contactmissionxlamc%40gmail.com&ctz=America%2FLos_Angeles" role="button">Calendar</a>
+        </ul>
+      </div>
+    </div>
+  </nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,114 @@
+<nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+            <img src="/wideWhiteLogo.png" width="100px" height="35px" alt="Mission X">
+          </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/competitions">Competitions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/cyberchats">Cyber Chats</a>
+          </li>
+          <a class="btn btn-primary" href="https://calendar.google.com/calendar/embed?src=contactmissionxlamc%40gmail.com&ctz=America%2FLos_Angeles" role="button">Calendar</a>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="p-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-6">
+        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h1 class="display-4 fw-bold lh-1">Mission X <br /> Cyber Security</h1>
+            <p class="lead">
+                Inclusive Individual and Team
+                <br />
+                Competative Cyber Experience
+            </p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 fw-bold">
+                    Learn More
+                </button>
+            </div>
+        </div>
+
+        <div class="col-lg-4 px-9">
+            <img class="rounded w-100" src="/main_background.jpg" alt="" />
+        </div>
+    </div>
+</div>
+<hr>
+ <!-- Section: Introduction -->
+ <section id="intro" class="intro text-center">
+    <div class="overlay">
+        <div class="intro-content">
+
+            <img class="animated fadeInDown delay-05s" src="/Isotipo_Transp.Color.png" width="10%" height="10%"  <span class="element"></span>
+            <p class="subtitle animated fadeInUp delay-05s">Simply Safe. Our Mission.</p>
+            <br>
+            <br>
+            <br>
+            <a class="fa fa-angle-down page-scroll"title="scroll down" href="/about">CyberChats</a>
+            
+        </div>
+    
+    </div>
+</section>
+
+<!-- Section: who we are / join the club -->
+<section id="about" class="text-center">
+    <div class="overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 wp1">
+                  <br/>
+                  <br/>
+                  <br/>
+                  <br/>
+                  <!--<img src="img/" id="logo-image" />-->
+                  <h2>About Us</h2>
+                    <p>
+                        Since 2018, the Cyber Security Club at Los Angeles Mission College has provided resources to its members and community to learn and excel in cybersecurity means.
+                        We provide our members with extensive networking opportunities that are beneficial both educationally and professionally.
+                        Our mission is to create a thrilling environment to be both informed and experienced with scenarios seen in the real world.
+                    </p>
+                    <p><b>Please follow our social media for the most up to date information!</b></p>
+                    <div><img src="img/arrow1.png" alt="Arrow" width="10%" height="10%" style="position: absolute; left: 55%"></div>
+                    <ul class="list-inline social-buttons animated fadeIn">
+                        <li><a id="instagram" title="Instagram" href="/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+          </div>
+</section>
+  <style>
+    .element{
+      size: 20px;
+    }
+    .nav-link{
+        color: white;
+    }
+    .nav-item :hover{
+        color: rgb(0,120,255);
+    }
+    .text-center, .overlay,.container{
+      color: rgb(0,120,255);
+      padding-left: 100px;
+      margin: center;
+    }
+  </style>
