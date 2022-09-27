06463d84e59d019ee7133317ed6d00c929ce1bfd
daniixani <89828237+daniixani@users.noreply.github.com>
2022-09-26 23:19:06 -0700

    website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,49 @@
-<script>import "../app.scss";</script><slot></slot>
-<nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Mission X</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/competitions">Competitions</a>
-          </li>
-          <li class="nav=item">
-            <a class="nav-link" href="/cyberchats">Cyber Chats</a>
-          </li>
-          <a class="btn btn-primary" href="https://calendar.google.com/calendar/embed?src=contactmissionxlamc%40gmail.com&ctz=America%2FLos_Angeles" role="button">Calendar</a>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<script>
+	import '../app.scss';
+</script>
+
+<nav class="navbar navbar-expand-lg bg-dark">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/">
+			<img src="/wideWhiteLogo.png" width="100px" height="35px" alt="Mission X" />
+		</a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon" />
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="/about">About Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/competitions">Competitions</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/cyberchats">Cyber Chats</a>
+				</li>
+				<a
+					class="btn btn-primary"
+					href="https://calendar.google.com/calendar/embed?src=contactmissionxlamc%40gmail.com&ctz=America%2FLos_Angeles"
+					role="button">Calendar</a
+				>
+			</ul>
+		</div>
+	</div>
+</nav>
+<slot />
+
+<style>
+	.nav-link {
+		color: white;
+	}
+	.nav-item :hover {
+		color: rgb(0, 120, 255);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">
-            <img src="/wideWhiteLogo.png" width="100px" height="35px" alt="Mission X">
-          </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/competitions">Competitions</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/cyberchats">Cyber Chats</a>
-          </li>
-          <a class="btn btn-primary" href="https://calendar.google.com/calendar/embed?src=contactmissionxlamc%40gmail.com&ctz=America%2FLos_Angeles" role="button">Calendar</a>
-        </ul>
-      </div>
-    </div>
-  </nav>
+
   <div class="p-5">
     <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-6">
         <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
@@ -57,7 +26,7 @@
     <div class="overlay">
         <div class="intro-content">
 
-            <img class="animated fadeInDown delay-05s" src="/Isotipo_Transp.Color.png" width="20%" height="30%">  <span class="element"></span>
+            <img class="intro-text" src="/Isotipo_Transp.Color.png" width="20%" height="30%">
             <p class="subtitle-intro">Simply Safe. Our Mission.</p>
             <br>
             <br>
@@ -74,7 +43,7 @@
     <div class="overlay">
         <div class="container">
             <div class="row">
-                <div class="col-md-8 col-md-offset-2 wp1">
+                <div class="about-content">
                   <br/>
                   <br/>
                   <br/>
@@ -87,7 +56,7 @@
                         Our mission is to create a thrilling environment to be both informed and experienced with scenarios seen in the real world.
                     </p>
                     <p><b>Please follow our social media for the most up to date information!</b></p>
-                    <div><img src="img/arrow1.png" alt="Arrow" width="10%" height="10%" style="position: absolute; left: 55%"></div>
+                    <div><img src="img/arrow1.png" alt="Arrow" width="10%" height="10%" ></div>
                     <ul class="list-inline social-buttons animated fadeIn">
                         <li><a id="instagram" title="Instagram" href="/" target="_blank"><i class="fab fa-instagram"></i></a></li>
                     </ul>
@@ -97,9 +66,7 @@
           </div>
 </section>
   <style>
-    .element{
-      size: 20px;
-    }
+    
     .nav-link{
         color: white;
     }
@@ -108,12 +75,14 @@
     }
     .text-center, .overlay,.container{
       color: rgb(0,120,255);
-      padding-left: 100px;
-      margin: center;
+      
+      text-align: center;
     }
     .intro-content{
-      padding-left: 35%;
-      padding-right: 15%;
-      margin: 50px;
+      text-align: center;
+    }
+    .about-content{
+      margin: auto;
+      max-width: 600px;
     }
   </style>
